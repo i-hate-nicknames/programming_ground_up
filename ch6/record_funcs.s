eca70bda72c020990842e4aa8ec69618a6acb509
nvm <nevermn@protonmail.com>
2020-04-25 15:30:56 +0300

    Add writing records

--- ch6/record_funcs.s
+++ ch6/record_funcs.s
@@ -1,5 +1,63 @@
-.globl smth
+.include "linux.s"
+.include "record-def.s"
 
-.section .data
-smth:
-    .byte 0
+
+# Purpose:  this function reads a record from the file
+#           descriptor
+# Input:    file descriptor and a buffer
+# Output:   status code after writing data to the buffer
+
+.equ ST_READ_BUFFER, 8
+.equ ST_FD_READ, 12
+
+.section .text
+
+.globl read_record
+.type read_record, @function
+
+read_record:
+    pushl %ebp
+    movl %esp, %ebp
+
+    # we use %ebx so we have to store previous value
+    pushl %ebx
+    movl ST_FD_READ(%ebp), %ebx
+    movl ST_READ_BUFFER, %ecx
+    movl $RECORD_SIZE, %edx
+    movl $SYS_READ, %eax
+    int $LINUX_SYSCALL
+
+    # %eax has return value which we will give back to our caller
+    # restore ebx
+    popl %ebx
+
+    movl %ebp, %esp
+    popl %ebp
+    ret
+
+# Purpose:  this function writes a record to the file
+#           descriptor
+# Input:    file descriptor and a buffer
+# Output:   status code after writing data to the fd
+
+.equ ST_WRITE_BUFFER, 8
+.equ ST_FD_WRITE, 12
+
+.globl write_record
+.type write_record, @function
+
+write_record:
+    pushl %ebp
+    movl %esp, %ebp
+    pushl %ebx
+
+    movl $SYS_WRITE, %eax
+    movl ST_FD_WRITE(%ebp), %ebx
+    movl ST_WRITE_BUFFER(%ebp), %ecx
+    movl $RECORD_SIZE, %edx
+    int $LINUX_SYSCALL
+
+    popl %ebx
+    movl %ebp, %esp
+    popl %ebp
+    ret
--- ch6/write_records.s
+++ ch6/write_records.s
@@ -1,13 +1,108 @@
 .include "linux.s"
 .include "record-def.s"
 
+.section .data
+
+# data of the records to write
+# each text data item is padded with null bytes to
+# occupy the fixed size specified in record-def.s
+
+# .rept is used for padding: basically it repeats
+# specified number of times whatever follows up until .endr
+
+record1:
+    .ascii "fredrick\0"
+    .rept 31 # padding up to 40 bytes
+    .byte 0
+    .endr
+
+    .ascii "Bartlett\0"
+    .rept 31
+    .byte 0
+    .endr
+
+    .ascii "4242 S Prairie\nTulsa, OK 55555\0"
+    .rept 209
+    .byte 0
+    .endr
+
+    .long 45 # age
+
+record2:
+    .ascii "Marilyn\0"
+    .rept 32 # padding up to 40 bytes
+    .byte 0
+    .endr
+
+    .ascii "Taylor\0"
+    .rept 33
+    .byte 0
+    .endr
+
+    .ascii "2224 S Johannan St\nChicago, IL 12345\0"
+    .rept 203
+    .byte 0
+    .endr
+
+    .long 29 # age
+
+record3:
+    .ascii "Derrick\0"
+    .rept 32 # padding up to 40 bytes
+    .byte 0
+    .endr
+
+    .ascii "McIterre\0"
+    .rept 31
+    .byte 0
+    .endr
+
+    .ascii "500 W Oakland\nSan Diego, CA 54321\0"
+    .rept 206
+    .byte 0
+    .endr
+
+    .long 31 # age
+
+file_name:
+    .ascii "records.dat\0"
+
 .section .text
+
+.equ ST_FD, -4
 
 .globl _start
 
 _start:
-    movl $1, %eax
-    movl %eax, smth
+    movl %esp, %ebp
+    subl $4, %esp
+
+    movl $SYS_OPEN, %eax
+    movl $file_name, %ebx
+    movl $0101, %ecx # create if it doesn't exist and open for writing
+    movl $0666, %edx
+    int $LINUX_SYSCALL
+    movl %eax, ST_FD(%ebp)
+
+    pushl ST_FD(%ebp)
+    pushl $record1
+    call write_record
+    addl $8, %esp
+
+    pushl ST_FD(%ebp)
+    pushl $record2
+    call write_record
+    addl $8, %esp
+
+    pushl ST_FD(%ebp)
+    pushl $record3
+    call write_record
+    addl $8, %esp
+
+    movl $SYS_CLOSE, %eax
+    movl ST_FD(%ebp), %ebx
+    int $LINUX_SYSCALL
+
     movl $SYS_EXIT, %eax
     movl $0, %ebx
     int $LINUX_SYSCALL
